c1f4de551286ab60e43c99b675acbc3b6ea14778
djy <djy1291923247@foxmail.com>
2022-03-13 10:18:30 +0800

    finish codelab5 Layout

---
+++ CodeLab/lab02/Affirmations/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.DataSource
+import com.example.affirmations.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize data.
+        val myDataSet = DataSource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycle_view)
+        recyclerView.adapter = ItemAdapter(this,myDataSet)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+    }
+}
